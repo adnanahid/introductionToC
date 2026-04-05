3392129c4a400dfcb1816baae1393d765896866a
Adnan <sikuahamed@gmail.com>
2026-04-05 14:38:59 +0600

    first prob done

--- practice.c
+++ practice.c
@@ -3,24 +3,24 @@
 Formula : $SI = \frac{P \times R \times T} {100} $
 Challenge : Print the result rounded to exactly 2 decimal places(e.g., 100.50).*/
 
-// #include <stdio.h>
-// int main()
-// {
-//     double principal_amount, total_interest, total_amount;
-//     float rate_of_interest, time_in_year;
+#include <stdio.h>
+int main()
+{
+    double principal_amount, total_interest, total_amount;
+    float rate_of_interest, time_in_year;
 
-//     scanf("%lf"
-//           "%f"
-//           "%f",
-//           &principal_amount, &rate_of_interest, &time_in_year);
+    scanf("%lf"
+          "%f"
+          "%f",
+          &principal_amount, &rate_of_interest, &time_in_year);
 
-//     total_interest = (principal_amount * rate_of_interest * time_in_year) / 100;
-//     total_amount = principal_amount + total_interest;
+    total_interest = (principal_amount * rate_of_interest * time_in_year) / 100;
+    total_amount = principal_amount + total_interest;
 
-//     printf("total amount %.2lf", total_amount);
+    printf("total amount %.2lf", total_amount);
 
-//     return 0;
-// }
+    return 0;
+}
 
 // 2. The ASCII "Spy" Program This will help you understand why the compiler says char is an int.
 /* Problem: Ask the user to enter a single character using %c.
@@ -47,14 +47,14 @@ Example Input: A → Output: Character: A, ASCII Value: 65.*/
 Formula: $F = (C \times \frac{9}{5}) + 32$
 Challenge: Try to write the fraction 9/5 in a way that C doesn't treat it as "1" (Integer Division). Use 9.0 / 5.0.*/
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main()
-{
-    float celcius;
-    scanf("%f", &celcius);
-    float fernhite;
-    fernhite = (celcius * (9.0 / 5.0)) + 32;
-    printf("temp in fernfite is %f", fernhite);
-    return 0;
-}
+// int main()
+// {
+//     float celcius;
+//     scanf("%f", &celcius);
+//     float fernhite;
+//     fernhite = (celcius * (9.0 / 5.0)) + 32;
+//     printf("temp in fernfite is %f", fernhite);
+//     return 0;
+// }
