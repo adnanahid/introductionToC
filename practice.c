6bf97dec00703f43bd74ffa72148cfaa073a600e
Adnan <sikuahamed@gmail.com>
2026-04-05 14:38:00 +0600

    second prob done

--- practice.c
+++ practice.c
@@ -3,43 +3,43 @@
 Formula : $SI = \frac{P \times R \times T} {100} $
 Challenge : Print the result rounded to exactly 2 decimal places(e.g., 100.50).*/
 
-#include <stdio.h>
-int main()
-{
-    double principal_amount, total_interest, total_amount;
-    float rate_of_interest, time_in_year;
+// #include <stdio.h>
+// int main()
+// {
+//     double principal_amount, total_interest, total_amount;
+//     float rate_of_interest, time_in_year;
 
-    scanf("%lf"
-          "%f"
-          "%f",
-          &principal_amount, &rate_of_interest, &time_in_year);
+//     scanf("%lf"
+//           "%f"
+//           "%f",
+//           &principal_amount, &rate_of_interest, &time_in_year);
 
-    total_interest = (principal_amount * rate_of_interest * time_in_year) / 100;
-    total_amount = principal_amount + total_interest;
+//     total_interest = (principal_amount * rate_of_interest * time_in_year) / 100;
+//     total_amount = principal_amount + total_interest;
 
-    printf("total amount %.2lf", total_amount);
+//     printf("total amount %.2lf", total_amount);
 
-    return 0;
-}
+//     return 0;
+// }
 
 // 2. The ASCII "Spy" Program This will help you understand why the compiler says char is an int.
 /* Problem: Ask the user to enter a single character using %c.
 Output: Print that character, then on the next line, print its Integer (ASCII) value using %d.
 Example Input: A → Output: Character: A, ASCII Value: 65.*/
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main()
-// {
+int main()
+{
 
-//     char alphabet;
-//     scanf("%c", &alphabet);
+    char alphabet;
+    scanf("%c", &alphabet);
 
-//     printf("the char is \"%c\" \n", alphabet);
-//     printf("the char ascii valuse is %d", alphabet);
+    printf("the char is \"%c\" \n", alphabet);
+    printf("the char ascii valuse is %d", alphabet);
 
-//     return 0;
-// }
+    return 0;
+}
 
 // 3. Temperature Converter (Celsius to Fahrenheit)Since you are a Physics student, you'll use these conversions often!
 
