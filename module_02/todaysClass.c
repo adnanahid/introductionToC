36caa28b019b545695b8b682b3a8b5f0a9407fc1
Adnan <sikuahamed@gmail.com>
2026-04-06 22:36:18 +0600

    solved quadrent problem

---
+++ module_02/todaysClass.c
@@ -0,0 +1,35 @@
+// problem 01: The "Quadrant" Finder (Physics/Math)
+// In Coordinate Geometry, a point (x,y) can be in one of four quadrants.
+// Problem: Take two integers x and y from the user.
+// Task: Use if...else if to determine where the point lies:
+// solution
+// #include <stdio.h>
+// int main()
+// {
+//     int x, y;
+//     scanf("%d %d", &x, &y);
+
+//     if (x > 0 && y > 0)
+//     {
+//         printf("Quadrant 01");
+//     }
+//     else if (x < 0 && y > 0)
+//     {
+//         printf("Quadrant 02");
+//     }
+//     else if (x < 0 && y < 0)
+//     {
+//         printf("Quadrant 03");
+//     }
+//     else if (x > 0 && y < 0)
+//     {
+//         printf("Quadrant 04");
+//     }
+//     else
+//     {
+//         printf("origin");
+//     }
+
+//     return 0;
+// }
+
