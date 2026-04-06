79249a6cb1ac35f4ac0f7d814be1fc8ef3fed32c
Adnan <sikuahamed@gmail.com>
2026-04-06 22:54:55 +0600

    the current bill problem solved

--- module_02/todaysClass.c
+++ module_02/todaysClass.c
@@ -33,3 +33,51 @@
 //     return 0;
 // }
 
+// problem 02: The "Short-Circuit" Max (Conditional Operator)
+// The Conditional Operator (condition ? true : false) is like a mini if...else that returns a value.
+// Problem: Take three numbers a, b, and c.
+// Task: Find the Maximum of the three using only one line of code with the Conditional Operator.
+// Hint: You can "nest" them: max = (a > b) ? ( (a > c) ? a : c ) : ( (b > c) ? b : c );
+// Goal: Don't use a single if statement!
+// solution
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b, c, max;
+//     scanf("%d %d %d", &a, &b, &c);
+//     max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+//     printf("the max num is %d", max);
+//     return 0;
+// }
+
+// problem 03: The "Electric Bill" Calculator (Tiered Logic)
+// This is a very common "real-world" interview problem.
+// Problem: Take the number of units of electricity consumed.
+// Task: Calculate the total bill based on these tiers:
+// First 50 units: 5 Taka/unit
+// Next 100 units (51-150): 7 Taka/unit
+// Above 150 units: 10 Taka/unit
+// Logic Trap: If someone uses 160 units, they pay $50 \times 5$ PLUS $100 \times 7$ PLUS $10 \times 10$. You can't just multiply the whole 160 by 10!
+// solution
+// #include <stdio.h>
+// int main()
+// {
+//     int units, bill;
+//     scanf("%d", &units);
+//     if (units > 0 && units <= 50)
+//     {
+//         bill = units * 5;
+//         printf("Your bill is %d", bill);
+//     }
+//     else if (units > 50 && units <= 150)
+//     {
+//         bill = 50 * 5 + (units - 50) * 7;
+//         printf("Your bill is %d", bill);
+//     }
+//     else
+//     {
+//         bill = 50 * 5 + 150 * 7 + (units - 150) * 10;
+//         printf("Your bill is %d", bill);
+//     }
+//     return 0;
+// }
