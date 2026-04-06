cf5afc8566394febead5fc5739a73ca898b22027
Adnan <sikuahamed@gmail.com>
2026-04-06 19:12:07 +0600

    solved todays first problem

---
+++ module_02/problem.c
@@ -0,0 +1,22 @@
+// problem 01: Take a number from user and check if its a even number or odd number.
+
+#include <stdio.h>
+int main()
+{
+    int the_number;
+    scanf("%d", &the_number);
+    if (the_number % 2 == 0)
+    {
+        printf("The number is even");
+    }
+    else
+    {
+        printf("The number is odd");
+    }
+    return 0;
+}
+
+// problem 02: Take a number from user and check if its a positive or negative number.
+
+
+// problem 03: Explain if else ladder.
