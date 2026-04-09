a38b051173333cd34ba2ae300a3a01f7bead88b6
Adnan <sikuahamed@gmail.com>
2026-04-09 08:49:00 +0600

    solved problem 4

--- module_03/loop.c
+++ module_03/loop.c
@@ -140,12 +140,32 @@
 //     return 0;
 // }
 
+// I know and you also know that you love practice day so much. So this task is for you. You will be given a positive integer N, you need to print "I Love Practice" N times.
+
+// Here positive integer means those integers that are greater than 0.
+// #include <stdio.h>
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     for (int i = 1; i <= n; i++) {
+//         printf("I Love Practice\n");
+//     }
+//     return 0;
+// }
+
+// You will be given a positive integer N, you need to print from 1 to N and besides the value, print Yes or No Print Yes if the value is divisible by 5 and print No otherwise.
+
 #include <stdio.h>
 int main() {
     int n;
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
-        printf("I Love Practice\n");
+        printf("%d ", i);
+        if (i % 5 == 0) {
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }
     }
     return 0;
 }
