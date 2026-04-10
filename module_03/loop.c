51d00efd17b2782c943e8b0e0a0a78b92a484488
Adnan <sikuahamed@gmail.com>
2026-04-10 11:20:23 +0600

    solved assignment

--- module_03/loop.c
+++ module_03/loop.c
@@ -170,12 +170,12 @@
 //     return 0;
 // }
 
-#include <stdio.h>
-int main() {
-    int i = 0, j = 0;
-    for (j = 1; j <= 5; j++) {
-        i++;
-    }
-    printf("%i\n", j);
-    return 0;
-}
+// #include <stdio.h>
+// int main() {
+//     int i = 0, j = 0;
+//     for (j = 1; j <= 5; j++) {
+//         i++;
+//     }
+//     printf("%i\n", j);
+//     return 0;
+// }
