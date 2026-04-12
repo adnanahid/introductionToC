eb7f06c56db671211575e8ee681df008f371f5a1
Adnan <sikuahamed@gmail.com>
2026-04-12 23:07:05 +0600

    no more today almost solved 25 problems

--- module_05/codeforces.c
+++ module_05/codeforces.c
@@ -91,16 +91,63 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main() {
+//     int days, years, months;
+//     scanf("%d", &days);
+//     years = days / 365;
+//     months = (days - (years * 365)) / 30;
+//     days = (days - (years * 365) - (months * 30));
+//     printf("%d years\n", years);
+//     printf("%d months\n", months);
+//     printf("%d days\n", days);
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//     float X;
+//     scanf("%f", &X);
+//     if (X >= 0 && X <= 25)
+//         printf("Interval [0,25]");
+//     else if (X > 25 && X <= 50)
+//         printf("Interval (25,50]");
+//     else if (X > 50 && X <= 75)
+//         printf("Interval (50,75]");
+//     else if (X > 75 && X <= 100)
+//         printf("Interval (75,100]");
+//     else
+//         printf("Out of Intervals");
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main() {
-    int days, years, months;
-    scanf("%d", &days);
-    years = days / 365;
-    months = (days - (years * 365)) / 30;
-    days = (days - (years * 365) - (months * 30));
-    printf("%d years\n", years);
-    printf("%d months\n", months);
-    printf("%d days\n", days);
+    int A, B, C, max, mid, min;
+    scanf("%d %d %d", &A, &B, &C);
+    max = A > B ? A > C ? A : C : B > C ? B
+                                        : C;
+    min = A < B ? A < C ? A : C : B < C ? B
+                                        : C;
+
+    if (max == A && min == B) {
+        mid = C;
+    } else if (max == A && min == C) {
+        mid = B;
+    } else if (max == B && min == A) {
+        mid = C;
+    } else if (max == B && min == C) {
+        mid = A;
+    } else if (max == C && min == A) {
+        mid = B;
+    } else if (max == C && min == B) {
+        mid = A;
+    }
+    printf("%d\n%d\n%d\n\n", min, mid, max);
+    printf("%d\n%d\n%d\n\n", A, B, C);
     return 0;
 }
